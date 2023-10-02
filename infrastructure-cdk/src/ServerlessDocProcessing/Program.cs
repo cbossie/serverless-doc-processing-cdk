3097cfbfdb353948a05ee46d08fac79c5eab6167
cbossie <cbossie@gmail.com>
2023-10-01 22:28:12 -0400

    RemoveAzurePipeline

--- infrastructure-cdk/src/ServerlessDocProcessing/Program.cs
+++ infrastructure-cdk/src/ServerlessDocProcessing/Program.cs
@@ -4,7 +4,7 @@ var app = new App();
 
 
 string environmentName = $"{app.Node.TryGetContext("environmentName")}" ?? "dev";
-string stackName = $"{app.Node.TryGetContext("stackName")}" ?? $"ServerlessDocProcessingStack-{environmentName}";
+string stackName = $"{app.Node.TryGetContext("stackName")}-{environmentName}";
 string account = $"{app.Node.TryGetContext("account")}";
 string region = $"{app.Node.TryGetContext("region")}";
 string functionBaseDir = $"{app.Node.TryGetContext("functionBaseDirectory")}";
@@ -15,20 +15,5 @@ new ServerlessDocProcessingStack(app, "ServerlessDocProcessingStack", new Server
     StackName = stackName ?? "ServerlessDocProcessing",
     EnvironmentName = environmentName ?? "dev",
     FunctionCodeBaseDirectory = functionBaseDir ?? "../functions"
-//    Env = makeEnv(account, region)
 });
 app.Synth();
-
-
-Amazon.CDK.Environment makeEnv(string account = null, string region = null)
-{
-    return new Amazon.CDK.Environment
-    {
-        Account = account ??
-            System.Environment.GetEnvironmentVariable("CDK_DEPLOY_ACCOUNT") ??
-            System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
-        Region = region ??
-            System.Environment.GetEnvironmentVariable("CDK_DEPLOY_REGION") ??
-            System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION")
-    };
-}
