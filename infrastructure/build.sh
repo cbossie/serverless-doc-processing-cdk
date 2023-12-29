d473eca4de4e71507ae19e5fa2d47ac26bbf49e7
cbossie <cbossie@gmail.com>
2023-12-29 00:01:53 -0500

    FIx Build Script

--- infrastructure/build.sh
+++ infrastructure/build.sh
@@ -1,5 +1,5 @@
 dotnet lambda package -pl ../functions/src/ProcessingFunctions/ -o ./function-output/ProcessingFunctions.zip
 dotnet lambda package -pl ../functions/src/SubmitToTextract/ -o ./function-output/SubmitToTextract.zip
-dotnet lambda package -pl ../functions/src/ProcessTextractResults/ -o ./function-output/ProcessTextractResults.zip
+dotnet lambda package -pl ../functions/src/ProcessTextractResults/ -o ./function-output/ProcessTextractQueryResults.zip
 dotnet lambda package -pl ../functions/src/RestartStepFunction/ -o ./function-output/RestartStepFunction.zip
 dotnet lambda package -pl ../functions/src/ProcessTextractExpenseResults/ -o ./function-output/ProcessTextractExpenseResults.zip
